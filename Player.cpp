2b579a26c5e41202b2faa11c6e5ef8456ed8b8c4
tw4706 <towajobseach@gmail.com>
2025-08-01 16:55:28 +0900

    up

--- Player.cpp
+++ Player.cpp
@@ -20,6 +20,10 @@ namespace
 	constexpr float kDefaultRadius = 16.0f;
 	// プレイヤーの移動速度
 	constexpr int  kSpeed = 2;
+	// 重力
+	constexpr float kGravity = 1.5f;
+	// 地面の当たり判定
+	constexpr int kGround = 530;
 
 }
 
@@ -34,11 +38,12 @@ Player::Player():
 
 Player::~Player()
 {
+
 }
 
-void Player::Init(int _padType, Vec2 _firstPos,int handle)
+void Player::Init(int _padType, Vec2 _firstPos,int _handle)
 {
-	m_handle = handle;
+	m_handle = _handle;
 	m_pos = _firstPos;
 	m_padType = _padType;
 	m_radius = kDefaultRadius;
@@ -51,11 +56,19 @@ void Player::End()
 
 void Player::Update()
 {
+	
 	// アニメーションの更新
 	m_animFrame++;
 	if (m_animFrame >= kIdleAnimNum * kAnimWaitFrame)
 	{
 		m_animFrame = 0;
+	}
+
+	Gravity();
+
+	if (m_pos.y >= kGround)
+	{
+		m_pos.y = kGround;
 	}
 
 	//	コントローラーのボタンの押された状態を取得する
@@ -65,10 +78,12 @@ void Player::Update()
 	if (input & PAD_INPUT_LEFT)
 	{
 		m_pos.x -= kSpeed;
+		m_isTurn = true; // 左に移動したら向きを左にする
 	}
 	if (input & PAD_INPUT_RIGHT)
 	{
 		m_pos.x += kSpeed;
+		m_isTurn = false; // 右に移動したら向きを右にする
 	}
 }
 
@@ -80,12 +95,34 @@ void Player::Draw()
 	// アニメーションの進行に合わせてグラフィックの切り取り位置を変更する
 	int srcX = kGraphWidth * animNum;
 	int srcY = 0;
+	if (m_padType==DX_INPUT_PAD1)
+	{
+		DrawRectGraph(m_pos.x - kGraphWidth / 2,
+			m_pos.y - kGraphHeight / 2,
+			srcX, srcY,
+			kGraphWidth, kGraphHeight,
+			m_handle, true, false
+		);
+	}
+	else
+	{
+		DrawRectGraph(m_pos.x - kGraphWidth / 2,
+			m_pos.y - kGraphHeight / 2,
+			srcX, srcY,
+			kGraphWidth, kGraphHeight,
+			m_handle, true, true
+		);
+	}
+	/*DrawRectGraph(m_pos.x - kGraphWidth / 2,
+				m_pos.y - kGraphHeight / 2,
+				srcX, srcY,
+				kGraphWidth, kGraphHeight,
+				m_handle, true,true
+				);*/
+}
 
-	DrawRectGraph(m_pos.x - kGraphWidth / 2,
-		m_pos.y - kGraphHeight / 2,
-		srcX, srcY,
-		kGraphWidth, kGraphHeight,
-		m_handle, true, m_isTurn
-	);
-//	DrawCircle(m_pos.x, m_pos.y, m_radius, GetColor(255, 255, 0),true);
+void Player::Gravity()
+{
+	// 重力を追加する
+	m_pos.y += kGravity;
 }
--- SceneMain.cpp
+++ SceneMain.cpp
@@ -1,30 +1,40 @@
 #include "SceneMain.h"
 #include"Dxlib.h"
 #include"Game.h"
-#include "Ground.h"
+#include "Bg.h"
 
 SceneMain::SceneMain():
 	m_player1GraphHandle(-1),
 	m_player2GraphHandle(-1),
+	m_player1AttackGraphHandle(-1),
+	m_player2AttackGraphHandle(-1),
 	m_player1(nullptr),
-	m_player2(nullptr)
+	m_player2(nullptr),
+	m_bg(nullptr)
 {
 
 }
 
 SceneMain::~SceneMain()
 {
-
 }
 
 void SceneMain::Init()
 {
-	//m_player1 = new Player();	// プレイヤーのインスタンスを生成	
-	//m_player2 = new Player();	
-	m_player1GraphHandle = LoadGraph("data/Player1.idle.png");	// プレイヤー1のグラフィックを読み込み
-	m_player2GraphHandle = LoadGraph("data/Player2.idle.png");	// プレイヤー2のグラフィックを読み込み
-	m_player1->Init(DX_INPUT_PAD1,Vec2(240,480),m_player1GraphHandle);// 初期化
-	m_player2->Init(DX_INPUT_PAD2,Vec2(480, 480), m_player2GraphHandle);	// プレイヤーの初期化
+	// プレイヤーのインスタンスを生成	
+	m_player1 = new Player();	
+	m_player2 = new Player();
+	m_bg = new Bg();
+	//グラフィックの読み込み
+	m_player1GraphHandle = LoadGraph("data/Player1.idle.png");
+	m_player2GraphHandle = LoadGraph("data/Player2.idle.png");
+	m_player1AttackGraphHandle = LoadGraph("data/Player1.attack.png");
+	m_player2AttackGraphHandle = LoadGraph("data/Player2.attack.png");
+	// 初期化
+	m_player1->Init(DX_INPUT_PAD1,Vec2(400,480),m_player1GraphHandle);
+	m_player2->Init(DX_INPUT_PAD2,Vec2(800, 480), m_player2GraphHandle);
+	m_bg->Init();
+	
 	
 }
 
@@ -32,19 +42,20 @@ void SceneMain::End()
 {
 	m_player1->End();	// 終了処理
 	m_player2->End();	// プレイヤーの終了処理
+	m_bg->End();
 }
 
 void SceneMain::Update()
 {
+	// プレイヤーの処理の更新
 	m_player1->Update();
-	m_player2->Update();	// プレイヤーの更新処理
-	// グラフィックの解放
-	DeleteGraph(m_player1GraphHandle);
-	DeleteGraph(m_player2GraphHandle);
+	m_player2->Update();
+	m_bg->Update();
 }
 
 void SceneMain::Draw()
 {
 	m_player1->Draw();
 	m_player2->Draw();	// プレイヤーの描画処理
+	m_bg->Draw();
 }
